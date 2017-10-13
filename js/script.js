3b649090126a1809bd58b14b1f48425c44d0c057
William Lin <4998811+ganlanyuan@users.noreply.github.com>
2017-10-13 04:36:38 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var slider = tns({
+    container: '.test',
+    fixedWidth: 155,
+    controls: false,
+    gutter: 2,
+    responsive: {
+      960: {
+        fixedWidth: 157,
+        gutter: 20,
+      }
+    }
+  })
